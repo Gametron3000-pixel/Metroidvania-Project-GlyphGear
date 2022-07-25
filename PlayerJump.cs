6b405e1666b560e08542fe6a5a5439e767939976
Gametron3000-pixel <109835309+Gametron3000-pixel@users.noreply.github.com>
2022-07-25 10:39:03 -0500

    You can now Wahoo.
    He can now hop, but still can't do your taxes. I also fixed the line of symmetry of the animations.

--- PlayerJump.cs
+++ PlayerJump.cs
@@ -4,33 +4,70 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    [Header("Public Vars")]
+    [Header("Jump Details")]
     public float jumpForce;
-    public bool grounded;
-    private Rigidbody2D rb;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
 
-    [Header("Private Vars")]
+    [Header("Ground Details")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float radOCircle;
     [SerializeField] private LayerMask whatIsGround;
+    public bool grounded;
+
+    [Header("Components")]
+    private Rigidbody2D rb;
+
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpTimeCounter = jumpTime;
     }
 
     private void Update()
     {
+        //what it means to be grounded
         grounded = Physics2D.OverlapCircle(groundCheck.position,radOCircle,whatIsGround);
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+
         //use space and w to jump
         if (Input.GetButtonDown("Jump") && grounded)
         {
+            //Jump!
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedJumping = false;
+        }
+
+        //to keep jumping while button is held
+        if (Input.GetButton("Jump") && !stoppedJumping && (jumpTimeCounter > 0))
+        {
+            
+            
+            //Jump!
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+
+            //if we stop holding button
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
         }
     }
+
+    
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawSphere(groundCheck.position, radOCircle);
     }
 }
+
